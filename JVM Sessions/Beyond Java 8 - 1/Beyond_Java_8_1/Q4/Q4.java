f77c0aaf026e9084426500f3c04a007fefd1e5d5
Akash Kumar <akash.kumar5@tothenew.com>
2025-07-01 12:26:01 +0530

    UPDATE: Q4.java

--- JVM Sessions/Beyond Java 8 - 1/Beyond_Java_8_1/Q4/Q4.java
+++ JVM Sessions/Beyond Java 8 - 1/Beyond_Java_8_1/Q4/Q4.java
@@ -2,16 +2,15 @@ package Beyond_Java_8_1.Q4;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Q4 {
     public static void main(String[] args) {
-     List<Integer> list = List.of(1,2,3,4,5,6,7,8,9,10);
+     List<Integer> list = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).collect(Collectors.toUnmodifiableList());
+     System.out.println("Immutable Copy of list : "+list);
 
-     List<Integer> list2 = List.copyOf(list);
-
-     System.out.println("Immutable Copy of list : "+list2);
 
 
     }
